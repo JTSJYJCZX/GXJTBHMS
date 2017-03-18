b4dd8fe6b27456a7df52dbc2dd2210c8cdfa96ad
yongyang.cai <83735605@qq.com>
2017-03-18 10:13:07 +0800

    为加密策略抽象类加密方法添加必要注释

--- GxjtBHMS.Models/Encryption/AppEncryptionStrategy.cs
+++ GxjtBHMS.Models/Encryption/AppEncryptionStrategy.cs
@@ -2,6 +2,11 @@
 {
     public abstract class AppEncryptionStrategy
     {
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="plainText">明文</param>
+        /// <returns>密文</returns>
         public abstract string Encrypt(string plainText);
     }
 }
