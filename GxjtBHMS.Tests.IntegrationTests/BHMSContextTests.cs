2514cbded831c4990aeea89381e6ef7f2c78ca46
侯攀 <houpan135@outlook.com>
2017-03-20 16:16:48 +0800

    恢复正常

--- GxjtBHMS.Tests.IntegrationTests/BHMSContextTests.cs
+++ GxjtBHMS.Tests.IntegrationTests/BHMSContextTests.cs
@@ -5,7 +5,7 @@ namespace GxjtBHMS.Tests.IntegrationTests
     [TestClass]
     public class BHMSContextTests : DataBaseTest
     {
-        [TestMethod]//暂时不执行
+        //[TestMethod]//暂时不执行
         public void InitDatas_Success()
         {
         }
