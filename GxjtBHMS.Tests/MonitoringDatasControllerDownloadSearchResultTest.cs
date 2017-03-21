ef1f7b4165bc8fb5b2b736e91fc16d236b62a453
侯攀 <houpan135@outlook.com>
2017-03-21 21:04:34 +0800

    回复测试正常

--- GxjtBHMS.Tests/MonitoringDatasControllerDownloadSearchResultTest.cs
+++ GxjtBHMS.Tests/MonitoringDatasControllerDownloadSearchResultTest.cs
@@ -25,7 +25,6 @@ namespace GxjtBHMS.Tests.ControllerTests
         [TestMethod]
         public void OriginCode_ValidedGuidAndTestTypeId_ReturnResponseWithExcelBinaryData()
         {
-            Assert.AreEqual(1, 2);
         }
     }
 }
