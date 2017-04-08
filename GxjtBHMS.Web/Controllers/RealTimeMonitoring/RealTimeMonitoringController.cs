fd62a63a86878c5983ae745443f7af946f9839da
Administrator <Administrator@SKY-20160823KEF>
2017-04-08 15:44:11 +0800

    重新构建实时监测菜单

---
+++ GxjtBHMS.Web/Controllers/RealTimeMonitoring/RealTimeMonitoringController.cs
@@ -0,0 +1,43 @@
+﻿using GxjtBHMS.Service.Interfaces;
+using System.Web.Mvc;
+
+namespace GxjtBHMS.Web.Controllers.RealTimeMonitoring
+{
+    public class RealTimeMonitoringController : Controller
+    {
+        public ActionResult SteelArchStrainDatasRealTimeMonitoring()
+        {
+            return View();
+        }
+
+        public ActionResult SteelLatticeStrainDatasRealTimeMonitoring()
+        {
+            return View();
+        }
+
+        public ActionResult ConcreteStrainStrainDatasRealTimeMonitoring()
+        {
+            return View();
+        }
+
+        public ActionResult DisplacementDatasRealTimeMonitoring()
+        {
+            return View();
+        }
+
+        public ActionResult CableForceDatasRealTimeMonitoring()
+        {
+            return View();
+        }
+
+        public ActionResult HumidityDatasRealTimeMonitoring()
+        {
+            return View();
+        }
+
+        public ActionResult TemperatureDatasRealTimeMonitoring()
+        {
+            return View();
+        }
+    }
+}
