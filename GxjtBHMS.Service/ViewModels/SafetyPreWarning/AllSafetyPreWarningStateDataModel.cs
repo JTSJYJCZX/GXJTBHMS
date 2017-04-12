a09b327c00a843508785db07d735fa60434dd984
黄建初 <huangjian553@126.com>
2017-04-12 09:11:19 +0800

    000

---
+++ GxjtBHMS.Service/ViewModels/SafetyPreWarning/AllSafetyPreWarningStateDataModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GxjtBHMS.Service.ViewModels.SafetyPreWarning
+{
+    public class AllSafetyPreWarningStateDataModel
+    {
+        public string BridgeCurrentSafetyPreWarningState { get; set; }
+        public IEnumerable<SafetyPreWarningStateAndTotalTimesModel> SafetyPreWarningStateData { get; set; }
+    }
+}
---
+++ GxjtBHMS.Web/SafetyPreWarningHub/SafetyPreWarningStateHub.cs
@@ -0,0 +1,31 @@
+﻿using GxjtBHMS.DependencyInjection;
+using GxjtBHMS.Service.Interfaces;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GxjtBHMS.Web.SafetyPreWarningHub
+{
+    public class SafetyPreWarningStateHub:Hub
+    {
+        ISteelArchStrainRealTimeDatasService _realTimeDatasService;
+
+        public SafetyPreWarningStateHub()
+        {
+            _realTimeDatasService = new NinjectControllerFactory().GetInstance<ISteelArchStrainRealTimeDatasService>();
+        }
+
+        public void DisplayWarningSteelArchStrainDatas(int testTypeId)
+        {
+            var sectionIds = _realTimeDatasService.GetSectionIdsBy(testTypeId).ToArray();
+            for (int i = 0; i < 100; i++)
+            {
+                //Thread.Sleep(1000);
+                var models = _realTimeDatasService.GetWarningStrainDatasBy(sectionIds);
+                Clients.All.RealTimeDisplayDatas(models);
+            }
+        }
+    }
+}
