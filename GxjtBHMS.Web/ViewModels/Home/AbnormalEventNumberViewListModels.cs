d200713c9ca160b5ba9bdc4e60bc84384007ec71
侯攀 <houpan135@outlook.com>
2017-05-06 16:14:37 +0800

    完善主页

---
+++ GxjtBHMS.Web/ViewModels/Home/AbnormalEventNumberViewListModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GxjtBHMS.Web.ViewModels.Home
+{
+    public class AbnormalEventNumberViewListModels
+    {    
+        public AbnormalEventNumberViewListModels()
+        {
+            AbnormalEventNumberViewModels = new List<AbnormalEventNumberViewModel>();
+        }
+        public IEnumerable<AbnormalEventNumberViewModel> AbnormalEventNumberViewModels;
+
+
+
+    }
+}
+
